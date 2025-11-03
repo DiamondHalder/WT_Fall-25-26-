b5befd261058b80e1d8960b6b3b2fd3cf22bd71c
DiamondHalder <diamond.halder28@gmail.com>
2025-11-03 15:00:04 +0600

    HTML

---
+++ Mid/LabTask1/Registration.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+      Registration
+    </title>
+<head>
+    
+<body>
+    
+<h1 align="left" style="color: red; border-bottom: 3px solid red;">Student Registration Information</h1>
+
+    
+
+   
+Frist name: <br>
+<input type="text"><br><br>
+Last name:<br>
+<input type="text"><br><br>
+Student Id:<br>
+<input type="text"><br><br>
+Program/Major<br>
+<input type="text"><br><br>
+Depertment: <br>
+<input type="text"><br><br>
+Phone Number:<br>
+<input type="number"><br><br>
+University Email:<br>
+<input type="text"><br><br>
+Create Password(minimum 8 character):<br>
+<input type="text"><br><br>
+Confirm Password:<br>
+<input type="text"><br><br>
+
+Select Semester:
+<input type="radio" name="A">Summer 2024
+<input type="radio" name="A">Fall 2025
+<input type="radio" name="A">Spring 2025
+<input type="radio" name="A">Other/Special Term
+
+<br>
+
+Request Credit Load(minimum 15)<br>
+<input type="text"><br><br>
+
+<input type="checkbox">I require academic advising before final registration.<br>
+
+<h1 align="left" style="color: red; border-bottom: 3px solid red;">Course Selection</h1>
+
+<br>
+<input type="checkbox">Math 1201(Calculus).
+<input type="checkbox">CS 2105(Data Structure).
+<input type="checkbox">ECON 1001(Microeconomics).
+<input type="checkbox">PHY 1102(Physics Lab).
+
+<br>
+Comments/Special Requests:<br>
+<input type="text" style="height:200px; width:1000px;"><br><br>
+
+
+
+
+
+<input type="submit">
+<input type="reset">
+    
+<body>
+
+<html>
+
