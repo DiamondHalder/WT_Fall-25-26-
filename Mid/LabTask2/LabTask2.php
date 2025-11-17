6116c27684f1e8abd1351b528412a8ca533240da
DiamondHalder <diamond.halder28@gmail.com>
2025-11-17 11:33:19 +0600

    4

--- Mid/LabTask2/LabTask2.php
+++ Mid/LabTask2/LabTask2.php
@@ -7,20 +7,29 @@
      <body>
         <h1 align="center">Participant Registration</h1>
         <div>
-           Full Name:
-           <input type="text" id="name">
-            Email:
-            <input type="text" id="email">
-            Phone Number:
-            <input type="number" id="number">
-            Password:
-            <input type="password" id="password">
-            Confirm Password:
-            <input type="password" id="cpassword">
+           Full Name: <br>
+           <input type="text" id="name"> <br>
+            Email: <br>
+            <input type="text" id="email"><br>
+            Phone Number: <br>
+            <input type="number" id="number"><br>
+            Password: <br>
+            <input type="password" id="password"><br>
+            Confirm Password: <br>
+            <input type="password" id="cpassword"><br>
             <button type="submit">Register </button>
-            
+
         </div>
+        <div id="error"></div>
+        <div id="output"></div>
         
+        <div id="activity">
+            Activity Name: <br>
+            <input type="text" id="activity"> <br>
+            <button type="submit">Add Activity</button>
+
+            <div id="activitylist"></div>
+        </div>
 
 
      </body>
