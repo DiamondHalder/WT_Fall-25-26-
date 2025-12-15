e75bf94dc471849ccedd5ae3b851a4905fdfd85d
DiamondHalder <diamond.halder28@gmail.com>
2025-12-16 00:15:38 +0600

    update

---
+++ Project/krishi_bazaar/includes/header.php
@@ -0,0 +1,9 @@
+/* php session */
+
+<div class="navbar">
+    <div><strong>Krishi Bazaar - Admin Panel</strong></div>
+    <div>
+        <a href="">Profile</a>
+        <a href="">Logout</a>
+    </div>
+</div>
