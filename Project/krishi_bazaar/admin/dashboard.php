9ab57b03b26e2216b6432208745656b2e776ecc8
DiamondHalder <diamond.halder28@gmail.com>
2025-12-15 23:48:15 +0600

    update

--- Project/krishi_bazaar/admin/dashboard.php
+++ Project/krishi_bazaar/admin/dashboard.php
@@ -4,6 +4,35 @@
         <title>Admin Dashboard</title>
     </head>
     <body>
-        
+        <div class="sidebar">
+            <a href="">Dashboard</a>
+            <a href="">Manage Users</a>
+            <a href="">Orders</a>
+            <a href="">Approve Products</a>
+            <a href="">Activity</a>
+        </div>
+
+        <div class="content">
+            <h2>Dashboard</h2><br>
+            <div class="card-container">
+                <div class="card">
+                    <h3>Total Users</h3>
+                    <p>--</p>
+                </div>
+                <div class="card">
+                    <h3>Total Sellers</h3>
+                    <p>--</p>
+                </div>
+                <div class="card">
+                    <h3>Total Products</h3>
+                    <p>--</p>
+                </div>
+                <div class="card">
+                    <h3>Total Orders</h3>
+                    <p>--</p>
+                </div>
+            </div>
+
+        </div>
     </body>
 </html>
