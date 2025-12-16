ce4f984fd8f75324087745509a8f8c009413a99c
DiamondHalder <diamond.halder28@gmail.com>
2025-12-16 15:57:10 +0600

    update

--- Project/krishi_bazaar/admin/manage_users.php
+++ Project/krishi_bazaar/admin/manage_users.php
@@ -45,10 +45,74 @@
                 <table>
                     <tr>
                         <th>User ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                        <th>Status</th>
+                        <th>Action</th>
                     </tr>
-                </table>
+                    <tr>
+                        <td>1</td>
+                        <td>Diamond</td>
+                        <td>dia@gmail.com</td>
+                        <td>Seller</td>
+                        <td>Active</td>
+                        <td>
+                            <form method="post" style="display:inline;">
+                                <input type="hidden" name="user_id" value="1">
+                                <input type="submit" name="block" value="Block">
+                            </form>
 
+                            <form method="post" style="display:inline;">
+                                <input type="hidden" name="user_id" value="1">
+                                <input type="submit" name="delete" value="Delete">
+                            </form>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>2</td>
+                        <td>Rabbi</td>
+                        <td>ria@gmail.com</td>
+                        <td>Customer</td>
+                        <td>Blocked</td>
+                        <td>
+                            <form method="post" style="display:inline;">
+                                <input type="hidden" name="user_id" value="2">
+                                <input type="submit" name="unblock" value="Unblock">
+                            </form>
+
+                            <form method="post" style="display:inline;">
+                                <input type="hidden" name="user_id" value="2">
+                                <input type="submit" name="delete" value="Delete">
+                            </form>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>3</td>
+                        <td>Rifat</td>
+                        <td>rrrra@gmail.com</td>
+                        <td>Seller</td>
+                        <td>Active</td>
+                        <td>
+                            <form method="post" style="display:inline;">
+                                <input type="hidden" name="user_id" value="3">
+                                <input type="submit" name="block" value="Block">
+                            </form>
+
+                            <form method="post" style="display:inline;">
+                                <input type="hidden" name="user_id" value="3">
+                                <input type="submit" name="delete" value="Delete">
+                            </form>
+                        </td>
+                    </tr>
+
+
+                </table>
             </div>
+
         </div>
+        <?php include("../include/footer.php"); ?>
     </body>
 </html>
