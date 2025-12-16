4f95bce9a1dcf079f3f3b439dbe8312ff3993363
DiamondHalder <diamond.halder28@gmail.com>
2025-12-16 15:18:48 +0600

    update

---
+++ Project/krishi_bazaar/admin/manage_users.php
@@ -0,0 +1,33 @@
+# <?php
+# include("../includes/auth_check.php");
+# ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Manage Users</title>
+        <link rel="stylesheet" href="../assets/css/style.css">
+    </head>
+    <body>
+        <?php include("../include/header/php"); ?>
+
+        <div class="container">
+            <div class="sidebar">
+            <a href="dashboard.php">Dashboard</a>
+            <a href="manage_users.php">Manage Users</a>
+            <a href="orders.php">Orders</a>
+            <a href="approve_products">Approve Products</a>
+            <a href="activity_log.php">Activity</a>
+            </div>
+
+            <div class="content">
+                <h2>Manage Users</h2><br>
+
+                <?php
+                
+                ?>
+
+            </div>
+        </div>
+    </body>
+</html>
